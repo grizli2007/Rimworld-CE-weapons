7450f968fd0ddfd7de7243530aca82a346bf316a
grizli2007 <73541680+grizli2007@users.noreply.github.com>
2023-05-27 21:29:16 +0300

    Add files via upload

---
+++ CE_MyMod/CE_MyMod/ModExtension_CE_Weapon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using CombatExtended;
+
+
+
+namespace CE_MyMod
+{
+    public class ModExtension_CE_Weapon : DefModExtension
+    {
+    }
+}
